9ed27b74a5c4068e8b360e2444977081d7603f30
Carlos M. Martinez <carlos@xt6.us>
2016-08-18 17:36:50 -0300

    Commit

--- bind9-dns-usecases/restart_bind_instance.sh
+++ bind9-dns-usecases/restart_bind_instance.sh
@@ -1,22 +1,25 @@
+#!/bin/bash
 # Boot bind-based signer
 # WD=/vagrant
-WD=$(pwd)
+WD="$(pwd)"
 INSTANCE=$1
 CONT_NAME="cm2c_$INSTANCE"
-HOST_PORT="192.168.1.10:53"
+# HOST_PORT="127.0.0.1:53"
+HOST_PORT="5301"
 BIND_OPTS=""
+DOCKER="docker"
 # echo "# Booting bind instance $INSTANCE, with base $WD, mounted on /v"
 
 cd $WD
 
 # removing previous instances of the container, if any
 # echo "# removing previous instances of the container, if any"
-N=$(/usr/bin/docker rm -f $CONT_NAME)
+N=$($DOCKER rm -f $CONT_NAME)
 echo J=$J\;
 echo export J\;
 
 # echo "# starting new instance"
-J=$(/usr/bin/docker run -d --name=$CONT_NAME -p $HOST_PORT:5301 -p $HOST_PORT:5301/udp -v $WD:/v cm2c/basebind9:1.0 \
+J=$($DOCKER run -d --name=$CONT_NAME -p $HOST_PORT:5301 -p $HOST_PORT:5301/udp -v "$WD":/v cm2c/basebind9:1.0 \
  /opt/bbsigner/sbin/named -4 -g $BIND_OPTS -c /v/dfiles/$INSTANCE/named.conf)
 echo J=$J\;
 echo export J\;
