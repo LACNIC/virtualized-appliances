70728b3c95e45d6a06de35305efb76e0c357d1fb
Agustín <aguformoso@gmail.com>
2017-07-10 14:20:19 -0300

    fixing `yesterday` date param

--- dsc-influx-import.docker/pipeline.sh
+++ dsc-influx-import.docker/pipeline.sh
@@ -4,7 +4,7 @@ echo $@
 [ -z "$2" ] || DATE=$2
 [ -z "$3" ] || USR=$3
 
-[ "$DATE" == 'yesterday' ] && DATE=$(docker run -t -v $(pwd)/data:/data --rm lacniclabs/dsc2influx:1.0 date +%Y-%m-%d -d 'yesterday')
+[ "$DATE" == 'yesterday' ] && DATE=$(docker run -t -v $(pwd)/data:/data --rm lacniclabs/dsc2influx:1.0 date +%Y-%m-%d -d 'yesterday' | cut -c -10)
 
 USR=$USR DATE=$DATE PROFILE=$PROFILE ./fetch.sh && \
 DATE=$DATE PROFILE=$PROFILE ./process.sh  &>/dev/null && \
